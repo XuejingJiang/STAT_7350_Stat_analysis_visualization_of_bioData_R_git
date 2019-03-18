9a45d5202551ed0f2e781b8027449c673d522cd4
Xuejing Jiang <eastern.lava@gmail.com>
2019-03-17 21:13:29 -0500

    update

---
+++ Assignment1/scripts/data_prep20190317.R
@@ -0,0 +1,12 @@
+# import table ------------------------------------------------------------
+table1 <- read_csv("data/table_1.csv") #sorted by invasion_threat
+table2 <- read_csv("data/table_2.csv") #sorted by invasion_cost
+table3 <- read_csv("data/table_3.csv") #sorted by invasion_gdp_proportion
+table4 <- read_csv("data/table_4.csv") #sorted by invasion_cost (source countries)
+invasive_species <- read_csv("data/table_6.csv") #invasive species and impact percentage
+africa_species <- read_csv("data/africa_species.csv")
+
+rm(table6)
+
+species
+
